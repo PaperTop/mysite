6464693733dd1cb6d96bb18e942086e7ddc83389
jadenhuang88@gmail.com <jadenhuang88@gmail.com>
2026-05-31 23:50:04 -0700

    organize-pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Analytics } from "@vercel/analytics/next";
+import SiteShell from "./components/SiteShell";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -20,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full antialiased">
       <body className="min-h-full flex flex-col">
-        {children}
+        <SiteShell>{children}</SiteShell>
         <Analytics />
       </body>
     </html>
